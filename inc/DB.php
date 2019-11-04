72fc7d7ee8be273ea4603adb88b925cd43411f93
Md. Ruhul Amin <ruhulmath08@gmail.com>
2019-11-04 22:16:54 +0600

    Update DB.php

--- inc/DB.php
+++ inc/DB.php
@@ -1,8 +1,8 @@
 <?php
 $db["db_host"] = "sql12.freemysqlhosting.net";
 $db["db_user"] = "sql12310720";
-$db["db_pass"] = "ruhul && 01745077380";
-$db["db_name"] = "WWalV4k3dL";
+$db["db_pass"] = "WWalV4k3dL";
+$db["db_name"] = "sql12310720";
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
