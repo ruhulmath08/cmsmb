0b31a894962c207328bd437f8ea606ae191c91b5
Md. Ruhul Amin <ruhulmath08@gmail.com>
2019-11-04 19:20:51 +0600

    Update DB.php

--- inc/DB.php
+++ inc/DB.php
@@ -1,12 +1,19 @@
 <?php
 $db["db_host"] = "localhost";
-$db["db_user"] = "ruhulmath08";
-$db["db_pass"] = "ruhul && 01745077380";
-$db["db_name"] = "cmsmb";
+$db["db_user"] = "id11109952_ruhulmath08";
+$db["db_pass"] = "ruhul420";
+$db["db_name"] = "id11109952_cmsmb";
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
 }
 
 $connectionDB = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check connection
+if ($connectionDB) {
+   echo "Connect Successfully!!!";
+}else{
+echo "Cannot connect, Something wrong";
+}
 ?>
