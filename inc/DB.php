e7387ab2f3b8793f1c888b700ff0ba37f4fd14fa
Md. Ruhul Amin <ruhulmath08@gmail.com>
2019-11-04 16:27:51 +0600

    Update DB.php

--- inc/DB.php
+++ inc/DB.php
@@ -1,8 +1,8 @@
 <?php
 $db["db_host"] = "localhost";
-$db["db_user"] = "root";
-$db["db_pass"] = "";
-$db["db_name"] = "cms";
+$db["db_user"] = "id11109952_ruhulmath08";
+$db["db_pass"] = "ruhul && 01745077380";
+$db["db_name"] = "id11109952_cmsmb";
 
 foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
